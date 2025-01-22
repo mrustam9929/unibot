09fc17718913fe114856526811f8f6a1922fa6e5
Rustam <mrustam9929@gmail.com>
2025-01-22 18:10:19 +0500

    - поправка зависимостей

--- src/web/application.py
+++ src/web/application.py
@@ -5,25 +5,11 @@ from web.api.router import api_router
 
 
 def get_app() -> FastAPI:
-    """
-    Get FastAPI application.
-
-    This is the main constructor of an application.
-
-    :return: application.
-    """
     app = FastAPI(
         title="UniBot",
         version='0.0.0',
         docs_url="/api/docs",
         default_response_class=UJSONResponse,
     )
-
-    # Adds startup and shutdown events.
-    # register_startup_event(app)
-    # register_shutdown_event(app)
-
-    # Main router for the API.
     app.include_router(router=api_router, prefix="/api")
-
     return app
